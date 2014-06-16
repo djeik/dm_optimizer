942761c1859e31782a9b5856276e69239363f079
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-06-16 14:24:52 -0400

    Still broken; crazy jumps everywhere, even in Simon's function.

--- dm.py
+++ dm.py
@@ -12,11 +12,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-verbose = False
-verbose2 = verbose or True
+verbosity = 1
 firstTargetRatio = 0.9
 scal = 0.15 #greediness
 pseudo = 0.0001
+
+def logmsg(priority, *args, **kwargs):
+    if priority <= verbosity:
+        print(*args, **kwargs)
+    else:
+        pass
 
 # These are the functions Simon defined to test it on:
 def simon_f1(xy):
@@ -27,33 +32,18 @@ def simon_f2(xs):
     xy = xs - [100, 100]
     return simon_f1(xy)
 
-#Find the n smallest values in xs
-def mins(xs_, n):
-    xs = list(xs_) # copy xs
-    nc = 0 # counter for the number of small values
-    while nc < n: # now we essentially do selection sort
-        for i in range(len(xs) - 1):
-            for j in range(i + 1, len(xs)):
-                if xs[j] < xs[i]:
-                    t = xs[i]
-                    xs[i] = xs[j]
-                    xs[j] = t
-                    nc += 1
-                    break
-
-    return xs[:n]
-
-#Find the n smallest values in xs, comparing by a given function
-def minsf(xs_, n, f):
+def mins(xs_, n, comp=lambda x: x):
+    """ Find the n smallest values in some indexable collection, comparing by a given function. The default such function is the identity function."""
     xs = list(xs_) # copy xs
     nc = 0 # counter for the number of small values
     for i in range(len(xs) - 1):
         for j in range(i + 1, len(xs)):
-            if f(xs[j]) < f(xs[i]):
+            if comp(xs[j]) < comp(xs[i]):
                 t = xs[i]
                 xs[i] = xs[j]
                 xs[j] = t
                 nc += 1
+                break
         if nc == n: break
 
     return xs[:n]
@@ -72,7 +62,7 @@ def nears(xs, target):
     elif len(xs) == 1: #Only one option.
         return xs[0]
 
-    if verbose: print("Finding nearest in list of " + str(len(xs)) + " elements.")
+    logmsg(2, "Finding nearest in list of " + str(len(xs)) + " elements.")
 
     # ns will hold our 2 values. We simply pick the two first ones from the input list.
     ns = map((lambda x: (x, norm(target - x))), xs[:2])
@@ -97,110 +87,101 @@ def newTarget(vals, best):
 # minima so far found.
 def refreshTarget(vals):
     # get the two best minima so far found
-    bests = minsf(vals,2,fst) #compare on y value in vals. Recall: vals :: [(y,x)]
+    bests = mins(vals,2,fst) #compare on y value in vals. Recall: vals :: [(y,x)]
     # bests :: [(y,x)], too !
-    if verbose2: print("Two best minima so far: ", bests[0], " and ", bests[1])
+    logmsg(2, "Two best minima so far: ", bests[0], " and ", bests[1])
     diff = bests[0][0] - bests[1][0]
-    if verbose2: print("Y-Difference between the minima: ", diff)
-    if diff > 0:
-        print("Warning: difference between two best minima is nonnegative!")
+    logmsg(2, "Y-Difference between the minima: ", diff)
     return bests[0][0] + scal * (-abs(diff))
 
-def nearestMinimum(vals, pmin, tol, fun):
-    # get the x-coordinates of the two previously-found minima closest to the current minimum
-    near = nears(map((lambda x: x[1]), vals), pmin) #map to extract x values (vals :: [(y,x)])
-
-    if near[0][1] < tol:
-        if verbose: print("Closest old minimum is too close. Choosing second-closest.")
-        nnear = near[1][0]
+def bestMinimum(prev_minima, target, fv, tol):
+    (y1, x1), (y2, x2) = mins(prev_minima, 2, comp=lambda (y, x): y - target)
+    if abs(y1-fv) < tol: # if the *best* minimum is the *current* minimum (fixed-point like behaviour)...
+        return (y2, x2) # return the *second*-best minimum.
     else:
-        nnear = near[0][0]
-
-    fnear = fun(nnear) # function-value at the minimum nearest to pmin
-    return (fnear, nnear)
-
-def bestMinimum(vals, target):
-    currentBestY, currentBestX = vals[0][0], vals[0][1]
-    for y, x in vals:
-        if abs(y - target) < abs(currentBestY - target):
-            currentBestY, currentBestX = y, x
-
-    return currentBestY, currentBestX
+        return (y1, x1)
 
 # perform the optimization
 def down(fun, niter, tol, x0, x1):
-    if verbose: print("Starting positions: " + str(x0) + ", " + str(x1) + ".")
+    logmsg(2, "Starting positions: " + str(x0) + ", " + str(x1) + ".")
 
     #fcs = 0 # total number of function calls required.
 
     # mathematica FindMinimum returns {f(x), x}, whereas fmin returns {x, f(x)}.
     # So simon's sol1[1] is fun(x0) in this case.
-    xv1 = fmin(fun, x0, disp=verbose)
-    fv1 = fun(xv1)
-    if verbose:
-        print("first minimum value: " + str(fun(x0)))
-        print("Second minimum value: " + str(fv1))
+    x0min = fmin(fun, x0, disp=(verbosity==2))
+    f0 = fun(x0min)
+    x1min = fmin(fun, x1, disp=(verbosity==2))
+    f1 = fun(x1min)
 
-    vals = [(fun(x0), x0), (fv1, xv1)]
-    target = firstTargetRatio * fv1
-    if verbose2: print("Initialized target to " + str(target))
+    logmsg(1, "first minimum value: ", f0)
+    logmsg(1, "Second minimum value: ", f1)
+
+    vals = [(f1, x1min), (f0, x0min)]
+    target = firstTargetRatio * f1
+    logmsg(1, "Initialized target to " + str(target))
 
     nx1 = x1
     lpos = [(fun(nx1), nx1)]
 
-    if verbose: print("Entering loop.")
+    logmsg(2, "Entering loop.")
 
     try:
         for i in xrange(niter):
-            if verbose: print("Guess point for this iteration: ", nx1)
-            pmin = fmin(fun, nx1, disp=verbose) #pmin is the x of the local minimum found in this iteration
+            logmsg(2, "Guess point for this iteration: ", nx1)
+            pmin = fmin(fun, nx1, disp=(verbosity==2)) #pmin is the x of the local minimum found in this iteration
             fv = fun(pmin) #fv is the function-value at that local minimum
-            if verbose: print("Found new local minimum f", pmin, " = ", fv, sep='')
+            logmsg(2, "Found new local minimum f", pmin, " = ", fv, sep='')
 
             delta = fv - target # how far are we from the target y value
 
-            if verbose: print("Got delta: " + str(delta))
+            logmsg(2, "Got delta: " + str(delta))
 
             if delta < 0: # we've fallen below the target, so we must update it.
                 target = newTarget(vals, fv)
-                if verbose2: print("Target updated to " + str(target))
+                logmsg(1, "Target updated to " + str(target))
                 delta = fv - target #recalculate delta for the new target
-                if verbose: print("New delta: " + str(delta))
+                logmsg(2, "New delta: " + str(delta))
 
-            if verbose: print("Target y-value ", target)
+            logmsg(2, "Target y-value ", target)
 
             ## wait this is no good...
             #fnear, nnear = nearestMinimum(vals, pmin, tol, fun)
 
             # let's take the *best* minimum instead.
-            fnear, nnear = bestMinimum(vals, target)
+            ynear, xnear = bestMinimum(vals, target, fv, tol)
 
-            if verbose: print("Nearest old minimum f", nnear, " = ", fnear, sep='')
+            logmsg(2, "Nearest old minimum f", xnear, " = ", ynear, sep='')
 
-            deltan = fnear - target # how far away is the best previously-found local minimum
+            if fv < ynear: #if the current local minimum is *better* that any previous one, we move *away* from the old one
+                logmsg(1, "Doubling back!")
+                stepdir = xnear - pmin
+            else: # otherwise, we move *towards* the old one
+                stepdir = pmin - xnear
 
-            if verbose: print("Got deltan: ", deltan)
+            deltay_prev = ynear - target # how far away is the best previously-found local minimum
+            step = -delta / (pseudo if (delta - deltay_prev)**2 < pseudo**2 else (delta - deltay_prev)) * stepdir
 
-            stepdir = pmin - nnear
+            logmsg(2, "Step endpoints: ", pmin, " and ", xnear)
 
-            step = -delta / (pseudo if (delta - deltan)**2 < pseudo**2 else (delta - deltan)) * stepdir
-
-            if verbose: print("Step endpoints: ", pmin, " and ", nnear)
-
-            nx1 += step
+            nx1 = nx1 + step
             lpos.append((fun(nx1), copy(nx1)))
-            if verbose2: print("Took step ", step)
+            logmsg(2, "Took step ", step)
 
             vals.append((fv, pmin))
 
             if norm(step) < tol:
-                if verbose2: print("Found fixed point: f", pmin, " = ", fnear, sep='')
+                logmsg(1, "Found fixed point: f", pmin, " = ", ynear, sep='')
                 return pmin, fv, lpos
+            elif norm(step) > 100:
+                logmsg(1, "Taking very large step!")
+                logmsg(1, "delta = ", delta, "; deltay_prev = ", deltay_prev, "; pmin = ", pmin, "; xnear = ", xnear, sep='')
+
             if i % 10 == 0: # A CONSTANT ! Consider changing this.
                 oldtarget = target
                 target = refreshTarget(vals)
                 # target increase can occur during the first iteration or so.
-                if verbose2: print("Refreshed target to " + str(target))
+                logmsg(1, "Refreshed target to " + str(target))
                 # if ((oldtarget - target) / target)**2 > 0.01: # i.e. 0.1**2 # why?
     except KeyboardInterrupt:
         # Note: when running interactively, the second keyboard interrupt caught will just kill the interpreter.
@@ -218,7 +199,7 @@ def unwrapBench(f):
     return lambda x: f(x)[0]
 
 def test(f):
-    xmin, fv, lpos = down(f, 2500, 0.000001, randomGuess(2, 10), randomGuess(2, 10))
+    xmin, fv, lpos = down(f, 2500, 0.0001, randomGuess(2, 10), randomGuess(2, 10))
     print("Found global minimum f", xmin, " = ", fv, sep='')
     print("Steps taken: ")
     map((lambda p: print(" ", p)), lpos)
