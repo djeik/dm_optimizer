f91eba23a1711dd223d3cac0d75a7b8733682c6a
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-06-27 16:44:59 -0400

    Optimizer rewritten to use OOP. Bugs ironed out. Solves Ackley pretty reliably.

--- dm.py
+++ dm.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 from scipy.optimize import fmin
 import numpy as np
+from numpy.linalg import norm
 import random
 import math
 from math import sqrt, cos, sin, tan, exp
@@ -12,16 +13,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-verbosity = 1
-firstTargetRatio = 0.9
-scal = 0.15 #greediness
-pseudo = 0.0001
+from sortedcontainers import sortedlist as sl
 
-def logmsg(priority, *args, **kwargs):
-    if priority <= verbosity:
-        print(*args, **kwargs)
-    else:
-        pass
+import sys
+
 
 # These are the functions Simon defined to test it on:
 def simon_f1(xy):
@@ -32,6 +27,18 @@ def simon_f2(xs):
     xy = xs - [100, 100]
     return simon_f1(xy)
 
+def fst(xs):
+    return xs[0]
+
+def snd(xs):
+    return xs[1]
+
+# deap's benchmarking functions return the values as tuples with nothing in element 1,
+# so we need to unpack element 0 which is the actual function-value.
+def unwrapBench(f):
+    return lambda x: f(x)[0]
+
+# TODO replace this with sortedlists!
 def mins(xs_, n, comp=lambda x: x):
     """ Find the n smallest values in some indexable collection, comparing by a given function. The default such function is the identity function."""
     xs = list(xs_) # copy xs
@@ -48,185 +55,194 @@ def mins(xs_, n, comp=lambda x: x):
 
     return xs[:n]
 
-def fst(xs):
-    return xs[0]
+def randomGuess(dim, scale = 1):
+    return np.array([scale * random.uniform(0,1) - scale/2.0 for _ in xrange(dim)])
 
-def snd(xs):
-    return xs[1]
+class dm_optimizer:
+    """ An optimizer using the difference map-based algorithm.
+        Attributes:
+            max_iterations      -- the maximum number of local minima to find before giving up.
+            tolerance           -- the threshold above which we consider two points to be distinct.
+            verbosity           -- controls debug output; higher numbers = more messages.
+            first_target_ratio  -- determines how low the first target should be.
+            greediness          -- how quickly should the target value be lowered.
+            fun                 -- the objective function.
+            pseudo              -- ?
+            vals                -- the local minima collected along the way.
+            lpos                -- the path the optimizer has followed along the objective function.
 
-#Get 2 values in the list that are nearest to the target.
-# Returns a list of tuples [(x, distance)]
-def nears(xs, target):
-    if len(xs) == 0: #Can't deal with emptiness.
-        raise "Empty list"
-    elif len(xs) == 1: #Only one option.
-        return xs[0]
+        Notes:
+            vals is stored as a sorted list, since we frequently just need to get the n smallest values.
+            It is a list of tuples (y, x), which is convenient since Python tuples are totally ordered according
+            to their first element.
+            """
 
-    logmsg(2, "Finding nearest in list of " + str(len(xs)) + " elements.")
+    def __init__(self, fun, max_iter, tol=0.00001, verbosity=1, pseudo=0.00001, greediness=0.05, first_target_ratio=0.9):
+        self.max_iterations     = max_iter
+        self.tolerance          = tol
+        self.verbosity          = verbosity
+        self.first_target_ratio = first_target_ratio
+        self.greediness         = greediness
+        self.fun                = fun
+        self.pseudo             = pseudo
+        self.vals               = sl.SortedList()
+        self.lpos               = []
 
-    # ns will hold our 2 values. We simply pick the two first ones from the input list.
-    ns = map((lambda x: (x, norm(target - x))), xs[:2])
+    # A nice wrapper around the print function that will print to standard error only if the given priority is less
+    # than the verbosity
+    def logmsg(self, priority, *args, **kwargs):
+        if priority <= self.verbosity:
+            print(*args, file=sys.stderr, **kwargs)
+        else:
+            pass
 
-    for x in xs:
-        delta = norm(target - x)
-        if delta < ns[0][1]: # ns[0][0] will be *the* closest point.
-            ns[0] = (x, delta)
-        elif delta < ns[1][1]: # ns[1][0] will be the second closest point.
-            ns[1] = (x, delta)
+    # Find the past minimum whose function-value is closest to the given one.
+    def best_minimum_y(self, fv):
+        u = sorted([x for x in self.vals], key=lambda (y, _): abs(y - fv))
 
-    return ns
+        if len(u) < 2:
+            return u[0]
 
-def norm(v):
-    return sqrt(sum(v**2))
+        (y1, x1), (y2, x2) = u[:2]
+        if abs(y1-fv) < self.tolerance: # if the *best* minimum is the *current* minimum (fixed-point like behaviour)...
+            return (y2, x2) # return the *second*-best minimum.
+        else:
+            return (y1, x1)
 
-def newTarget(vals, best):
-    # get the minimum y value recorded in our list of minima, and push down from there.
-    return best + scal * (best - min(map((lambda v: v[0]), vals)))
+    def best_minimum_x(self, pmin):
+        u = sorted([x for x in self.vals], key=lambda (y, x): norm(pmin - x))
 
-# move the target lower, by adding to it the scaled difference between the two best
-# minima so far found.
-def refreshTarget(vals):
-    # get the two best minima so far found
-    bests = mins(vals,2,fst) #compare on y value in vals. Recall: vals :: [(y,x)]
-    # bests :: [(y,x)], too !
-    logmsg(2, "Two best minima so far: ", bests[0], " and ", bests[1])
-    diff = bests[0][0] - bests[1][0]
-    logmsg(2, "Y-Difference between the minima: ", diff)
-    return bests[0][0] + scal * (-abs(diff))
+        if len(u) < 2:
+            return u[0]
 
-def bestMinimum(prev_minima, target, fv, tol):
-    (y1, x1), (y2, x2) = mins(prev_minima, 2, comp=lambda (y, x): y - target)
-    if abs(y1-fv) < tol: # if the *best* minimum is the *current* minimum (fixed-point like behaviour)...
-        return (y2, x2) # return the *second*-best minimum.
-    else:
-        return (y1, x1)
+        (y1, x1), (y2, x2) = u[:2]
+        if norm(x1 - pmin) < self.tolerance: # if the best minimum *is* the current minimum
+            return (y2, x2) # then return the second-best minimum
+        else:
+            return (y1, x1)
 
-# perform the optimization
-def down(fun, niter, tol, x0, x1):
-    logmsg(2, "Starting positions: " + str(x0) + ", " + str(x1) + ".")
+    def new_target(self, best):
+        # get the minimum y value recorded in our list of minima, and push down from there.
+        self.target = best + self.greediness * (best - min(map((lambda v: v[0]), self.vals)))
 
-    #fcs = 0 # total number of function calls required.
+    def refresh_target(self):
+        # get the two best minima so far found
+        bests = self.vals[:2] # remember, vals is sorted in ascending order of the y-value.
+        # bests :: [(y,x)], too !
+        self.logmsg(1, "Two best minima so far: ", bests[0], " and ", bests[1])
+        diff = bests[0][0] - bests[1][0]
+        if diff > 0:
+            raise Exception("The difference between the two best minima should be negative!")
+        self.logmsg(2, "Y-Difference between the minima: ", diff)
+        self.target = bests[0][0] + self.greediness * diff
 
-    # mathematica FindMinimum returns {f(x), x}, whereas fmin returns {x, f(x)}.
-    # So simon's sol1[1] is fun(x0) in this case.
-    x0min = fmin(fun, x0, disp=(verbosity==2))
-    f0 = fun(x0min)
-    x1min = fmin(fun, x1, disp=(verbosity==2))
-    f1 = fun(x1min)
+    def minimize(self, x0, x1):
+        x0min = fmin(self.fun, x0, disp=self.verbosity==2)
+        f0 = self.fun(x0min)
 
-    logmsg(1, "first minimum value: ", f0)
-    logmsg(1, "Second minimum value: ", f1)
+        self.vals.add( (f0, x0min) )
+        self.target = self.first_target_ratio * f0
 
-    vals = [(f1, x1min), (f0, x0min)]
-    target = firstTargetRatio * f1
-    logmsg(1, "Initialized target to " + str(target))
+        nx1 = x1
+        self.lpos = [(self.fun(nx1), nx1)]
 
-    nx1 = x1
-    lpos = [(fun(nx1), nx1)]
+        self.logmsg(2, "Entering loop.")
 
-    logmsg(2, "Entering loop.")
+        try:
+            for i in xrange(self.max_iterations):
+                self.logmsg(2, "Guess point for this iteration:", nx1)
+                pmin = fmin(self.fun, nx1, disp = self.verbosity == 2) # the x value at the local minimum for this iteration
+                fv = self.fun(pmin) # the function value at the local minimum for this iteration
+                delta = fv - self.target
 
-    try:
-        for i in xrange(niter):
-            logmsg(2, "Guess point for this iteration: ", nx1)
-            pmin = fmin(fun, nx1, disp=(verbosity==2)) #pmin is the x of the local minimum found in this iteration
-            fv = fun(pmin) #fv is the function-value at that local minimum
-            logmsg(2, "Found new local minimum f", pmin, " = ", fv, sep='')
+                self.logmsg(2, "Got delta:", delta)
 
-            delta = fv - target # how far are we from the target y value
+                if delta < 0: # we've fallen below the target, so we must update it.
+                    self.new_target(fv) # to get the next target, all we need is the current function value and the list of minima so far
+                    self.logmsg(1, "Target updated to", self.target)
+                    delta = fv - self.target # recalculate delta for the new target
+                    self.logmsg(2, "New delta:", delta)
+                else:
+                    self.logmsg(2, "Target y-value", self.target)
 
-            logmsg(2, "Got delta: " + str(delta))
+                # let's take the *best* minimum instead, i.e. the minimum closest to the target
+                # wait that's no good. Simon's code has it taking the minimum closest to the current x-value!
+                ynear, xnear = self.best_minimum_x(pmin)
 
-            if delta < 0: # we've fallen below the target, so we must update it.
-                target = newTarget(vals, fv)
-                logmsg(1, "Target updated to " + str(target))
-                delta = fv - target #recalculate delta for the new target
-                logmsg(2, "New delta: " + str(delta))
+                self.logmsg(2, "Nearest old minimum f", xnear, " = ", ynear, sep='')
 
-            logmsg(2, "Target y-value ", target)
-
-            ## wait this is no good...
-            #fnear, nnear = nearestMinimum(vals, pmin, tol, fun)
-
-            # let's take the *best* minimum instead.
-            ynear, xnear = bestMinimum(vals, target, fv, tol)
-
-            logmsg(2, "Nearest old minimum f", xnear, " = ", ynear, sep='')
-
-            if fv < ynear: #if the current local minimum is *better* that any previous one, we move *away* from the old one
-                logmsg(1, "Doubling back!")
-                stepdir = xnear - pmin
-            else: # otherwise, we move *towards* the old one
+                #if fv < ynear: #if the current local minimum is *better* than any previous one, we move *away* from the old one
+                #    self.logmsg(1, "Doubling back!")
+                #    stepdir = xnear - pmin
+                #else: # otherwise, we move *towards* the old one
                 stepdir = pmin - xnear
 
-            deltay_prev = ynear - target # how far away is the best previously-found local minimum
-            step = -delta / (pseudo if (delta - deltay_prev)**2 < pseudo**2 else (delta - deltay_prev)) * stepdir
+                deltay_prev = ynear - self.target # how far away is the best previously-found local minimum
+                step = -delta / (self.pseudo if (delta - deltay_prev)**2 < self.pseudo**2 else (delta - deltay_prev)) * stepdir
 
-            logmsg(2, "Step endpoints: ", pmin, " and ", xnear)
+                self.logmsg(2, "Step endpoints: ", pmin, " and ", xnear)
 
-            nx1 = nx1 + step
-            lpos.append((fun(nx1), copy(nx1)))
-            logmsg(2, "Took step ", step)
+                nx1 += step # actually take the step
+                self.lpos.append((self.fun(nx1), copy(nx1))) # add the new position to
+                self.logmsg(2, "Took step ", step)
 
-            vals.append((fv, pmin))
+                self.vals.add( (fv, pmin) )
 
-            if norm(step) < tol:
-                logmsg(1, "Found fixed point: f", pmin, " = ", ynear, sep='')
-                return pmin, fv, lpos
-            elif norm(step) > 100:
-                logmsg(1, "Taking very large step!")
-                logmsg(1, "delta = ", delta, "; deltay_prev = ", deltay_prev, "; pmin = ", pmin, "; xnear = ", xnear, sep='')
+                if norm(step) < self.tolerance:
+                    self.logmsg(1, "Found fixed point: f", pmin, " = ", ynear, sep='')
+                    return pmin, fv, self.lpos
+                elif norm(step) > 100:
+                    self.logmsg(1, "Taking very large step!")
+                    self.logmsg(1, "delta = ", delta, "; deltay_prev = ", deltay_prev, "; pmin = ", pmin, "; xnear = ", xnear, sep='')
 
-            if i % 10 == 0: # A CONSTANT ! Consider changing this.
-                oldtarget = target
-                target = refreshTarget(vals)
-                # target increase can occur during the first iteration or so.
-                logmsg(1, "Refreshed target to " + str(target))
-                # if ((oldtarget - target) / target)**2 > 0.01: # i.e. 0.1**2 # why?
-    except KeyboardInterrupt:
-        # Note: when running interactively, the second keyboard interrupt caught will just kill the interpreter.
-        # note2: ipython doesn't die the second time.
-        print("Halting optimization.")
-
-    return sorted(vals)[0], fv, lpos
-
-def randomGuess(dim, scale = 1):
-    return np.array([scale * random.uniform(0,2) for q in xrange(dim)])
-
-# deap's benchmarking functions return the values as tuples with nothing in element 1,
-# so we need to unpack element 0 which is the actual function-value.
-def unwrapBench(f):
-    return lambda x: f(x)[0]
+                if i % 5 == 0: # A CONSTANT ! Consider changing this.
+                    oldtarget = self.target
+                    self.refresh_target() # mutates target
+                    self.logmsg(1, "Refreshed target to", self.target)
+                    # if ((oldtarget - target) / target)**2 > 0.01: # i.e. 0.1**2 # why?
+        except:
+            self.logmsg(-1, "An error occurred while optimizing.")
+            raise
 
 def test(f):
-    xmin, fv, lpos = down(f, 2500, 0.0001, randomGuess(2, 10), randomGuess(2, 10))
+    optimizer = dm_optimizer(f, 2000, 0.000001)
+    xmin, fv, lpos = optimizer.minimize(randomGuess(2, 2), randomGuess(2, 2))
     print("Found global minimum f", xmin, " = ", fv, sep='')
     print("Steps taken: ")
     map((lambda p: print(" ", p)), lpos)
     if len(xmin) == 2: #only plot if in 3D !
-        plotf(f, lpos, -10, 10)
+        plotf(f, lpos)
 
 def testAckley():
     test(unwrapBench(bench.ackley))
 
-def testSimon1():
-    test(simon_f1)
-
-# A visual debug tool, basically.
-def plotf(f, xyzs_, start=-10, end=10, smoothness=0.3):
+# Visual debug tool for 3d
+def plotf(f, xyzs_, start=np.array([-1,-1]), end=np.array([1,1]), smoothness=1.0, autobound=True, autosmooth=True):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    xs = ys = np.arange(start, end, smoothness)
-    X, Y = np.meshgrid(xs, ys)
-    zs = np.array([f((x,y)) for x,y in zip(np.ravel(X), np.ravel(Y))])
-    Z = zs.reshape(X.shape)
-    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, # draw the surface
-            linewidth=0, antialiased=False)
-    fig.colorbar(surf, shrink=0.5, aspect=5)
 
     z, xys = zip(*xyzs_) # prepare to draw the line
     x, y = zip(*xys)
     N = len(x)
     for i in xrange(N-1):
         ax.plot(x[i:i+2], y[i:i+2], z[i:i+2], color=plt.cm.jet(255*i/N))
+
+    if autobound:
+        start = reduce(np.minimum, xys)
+        end = reduce (np.maximum, xys)
+
+    if autosmooth:
+        smoothness = norm(end - start) / 100.0
+
+    print(start, end, smoothness)
+
+    xs = np.arange(start[0], end[0], smoothness)
+    ys = np.arange(start[1] + y[-1], end[1] + y[-1], smoothness)
+    X, Y = np.meshgrid(xs, ys)
+    zs = np.array([f((x,y)) for x,y in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, # draw the surface
+            linewidth=0, antialiased=True)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
     plt.show()
