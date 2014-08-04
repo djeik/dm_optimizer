ff29145925c1669cd453ad6470cedc6d50960eb6
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-04 10:35:56 -0400

    adjust results-watching script to use new results directory

--- watch-experiments.sh
+++ watch-experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmd='find experiments -name "averages.txt" -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" " | xargs -d\\n cat | column -ts ,'
+cmd='find results -name "averages.txt" -printf "%T@ %p\n" | sort -n | tail -1 | cut -f2- -d" " | xargs -d\\n cat | column -ts ,'
 
 watch "$cmd"
