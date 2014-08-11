715b27da77098a24ca30e628277229f2b9f276cd
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-11 17:13:58 -0400

    adjust wrapper script to use current time if no output dir is given

--- run_solved_vs_iterations.py
+++ run_solved_vs_iterations.py
@@ -3,13 +3,14 @@
 from __future__ import print_function
 from sys import argv as args
 from sys import exit
+from datetime import datetime
 import dm_tests as dmt
 
 if __name__ == "__main__":
     try:
         path = args[1]
     except:
-        print("Invalid command line arguments.")
-        exit(1)
+        path = str(datetime.now())
+
 
     dmt.solved_vs_iterations(path)
