2e50e832ae8cb8fe2e0bde18b28c037c45cf1013
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-14 15:59:01 -0400

    add script to submit a bunch of jobs to abacus for the stepsizes

---
+++ stepsizes-submit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import subprocess as sp
+import numpy as np
+
+from sys import stdout, stderr
+
+if __name__ == "__main__":
+    for stepscale in np.arange(0.1, 1.31, 0.1):
+        sp.call(["Qmsub", "-n", "5", "./run_solved_vs_iterations.py", "-s", str(stepscale)], stdout=stdout, stderr=stderr)
