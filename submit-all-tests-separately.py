5ca3f7b5f3fcb218d9bfcc673ffc7a310fb8081a
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-18 17:22:56 -0400

    generalize the abacus-submit script to run SA as well

--- submit-all-tests-separately.py
+++ submit-all-tests-separately.py
@@ -10,13 +10,19 @@ import os
 from os import path
 
 if __name__ == "__main__":
-    for optimizer in dmtc.optimizers.keys():
-        for test in dmtc.tests:
-            for sscale in arange(0.1, 1.21, 0.1):
-                sp.call(["Qmsub", "-n", str(dmtc.solved_vs_iterations_subproc_count),
-                                  "-h", "16",
-                                  "./run_solved_vs_iterations_inner.py",
-                                  "-o", "results/" + "all-dm/" + str(sscale),
-                                  "-s", str(sscale),
-                                  "-t", test["name"],
-                                  "--solver", optimizer])
+    for test in dmtc.tests:
+        sp.call(["Qmsub", "-n", str(dmtc.solved_vs_iterations_subproc_count),
+                          "-h", 16,
+                          "./run_solved_vs_iterations_inner.py",
+                          "-o",       "\"results/" + str(datetime.now()) + "/sa\"",
+                          "-t",       test["name"],
+                          "--solver", "sa"])
+
+        for sscale in arange(0.1, 1.21, 0.1):
+            sp.call(["Qmsub", "-n", str(dmtc.solved_vs_iterations_subproc_count),
+                              "-h", "16",
+                              "./run_solved_vs_iterations_inner.py",
+                              "-o",       "results/" + "all-dm/" + str(sscale),
+                              "-s",       str(sscale),
+                              "-t",       test["name"],
+                              "--solver", "dm"])
