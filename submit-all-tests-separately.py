2fb710f44f3a279a5d7604495b9d898bce0f5b87
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-18 17:34:58 -0400

    add datetime folder to path of dm output

--- submit-all-tests-separately.py
+++ submit-all-tests-separately.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             sp.call(["Qmsub", "-n", str(dmtc.solved_vs_iterations_subproc_count),
                               "-h", "16",
                               "./run_solved_vs_iterations_inner.py",
-                              "-o",       "results/" + "all-dm/" + str(sscale),
+                              "-o",       "\"results/" + str(datetime.now()) + "/dm\"",
                               "-s",       str(sscale),
                               "-t",       test["name"],
                               "--solver", "dm"])
