86effa69f8f4f7edfbcb3c75706fd66af09cdd84
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-08-27 10:51:23 -0400

    add docstrings

--- dm_optimizer.py
+++ dm_optimizer.py
@@ -108,6 +108,7 @@ class dm_optimizer:
         return res.x
 
     def calculate_step_scale(self, destination):
+        """ Calculate the step scale to use in the direction of a given destination in the search space. """
         return self.stepscale_constant
 
     def step_toward(self, destination):
@@ -125,6 +126,10 @@ class dm_optimizer:
         raise BestMinimumException("There are no past minima.")
 
     def step_to_best_minimum(self):
+        """ Calculate a step towards the best past minimum. For what "best" means, consult the documentation of
+            `get_best_minimum`. Note that the step is merely calculated, not taken. For that, pass the result of
+            this method to `take_step`.
+            """
         return self.step_toward(self.get_best_minimum())
 
     def fv_after_step_from(self, origin, step):
