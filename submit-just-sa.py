30a551a4b783fcbc6f44f8e5a831b2a2b08ba9ae
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-10-16 13:34:57 -0400

    make script to regenerate just the SA solved % vs time data

---
+++ submit-just-sa.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+""" Don't run this script !
+    It's a one-shot thing to regenerate SA output for success-vs-iterations
+    for the plots for the paper. Turns out that SA was run with 500 iterations, but
+    dm with only 250, so SA was unfairly outperforming us each time.
+    """
+
+import dm_tests_config as dmtc
+import os
+from os import path
+import subprocess as sp
+from sys import argv as args
+
+if __name__ == "__main__":
+    t = args[1]
+    for test in dmtc.tests:
+        sp.call(["Qmsub", "-n", str(dmtc.solved_vs_iterations_subproc_count),
+                          "-h", "16",
+                          "./run_solved_vs_iterations_inner.py",
+                          "-o",       "\"" + t + "/sa\"",
+                          "-t",       test["name"],
+                          "--solver", "sa"])
