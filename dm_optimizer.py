209e2721a7d77e9137affda497960a8071a29454
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-10-21 15:37:57 -0400

    switch to furthest-minimum strategy

--- dm_optimizer.py
+++ dm_optimizer.py
@@ -278,7 +278,7 @@ class dm_optimizer:
         return self.step_toward(
                 self.get_best_minimum(
                     self._distance_ratio_distinctness_strategy, # parameterize !
-                    self._exception_failure_strategy),
+                    self._furthest_minimum_failure_strategy),
                 self._constant_factor_reversing_steptake_strategy)
 
     def take_step(self, step):
