363968ddd56c27f964fcd4b16f28564781f4dd47
Jacob Errington <jacob.errington@mail.mcgill.ca>
2014-12-02 10:57:48 -0500

    move plotting function for stepscales out of dm_tests
    dm_tests is hell. Instead, I've moved it to the actual plotting script,
    rather that have that script import dm_tests just to call one function.
    
    There are also some improvements in the plotter, to deal with missing
    data. For instance, it is possible to run this plotting even if one of
    DM's or SA's data is missing.
    
    It seems broken, right now.

--- dm_tests.py
+++ dm_tests.py
@@ -579,64 +579,6 @@ def parse_solved_vs_iterations_data(data_dir, runs_count):
         # :: Map FunctionName ([FractionSolved1], [FractionSolved2])
     return data
 
-def solved_vs_iterations_plots_pro(path_to_data,
-        iterations_count=iterations_config["end"],
-        runs_count=experiment_defaults["runs"]):
-    # we have data per stepsize stored in all-dm, in folders named with the
-    # stepsize.
-    path_to_stepsizes = path.join(path_to_data, "dm")
-    stepsizes = map(
-            lambda s: (float(s),
-                       path.join(path_to_stepsizes, s, "dm", "results")),
-            os.listdir(path_to_stepsizes))
-
-    maxsize = max(imap(j.fst, stepsizes))
-
-    # make the directory where we'll save the plots
-    plot_dir = path.join(path_to_data, "plots")
-    j.mkdir_p(plot_dir)
-
-    # dict like D[stepsize][function_name] : list representing fraction
-    # completed by time
-    functions_by_stepsize = map(
-        lambda (stepsize, directory): (
-            stepsize,
-            parse_solved_vs_iterations_data_for_one_optimizer(
-                directory, runs_count)),
-        stepsizes)
-
-    # now we need to do the same but for SA
-    simulated_annealing_path = path.join(
-            path_to_data, "sa", "sa", "results")
-
-    sa_data = parse_solved_vs_iterations_data_for_one_optimizer(
-            simulated_annealing_path, runs_count)
-
-    for test in test_functions.tests_list:
-        if not (test["name"] in sa_data and all(
-                imap(
-                    lambda d: test["name"] in d,
-                    imap(
-                        project(1),
-                        functions_by_stepsize)))):
-            print("Skipping function", test["name"],
-                    "because one or more datasets don't have entries for it.")
-            continue
-        fig = plt.figure()
-        fig.suptitle("%s (%d dimensions) - "
-                "fraction of successful runs vs. time"
-                % (test["name"], test["dimensions"]))
-        ax = fig.add_subplot(1,1,1)
-        ax.set_xlim(0, iterations_count)
-        ax.set_ylim(0, 1)
-        ax.plot(sa_data[test["name"]], label="sa", color=(0, 0, 1))
-        for (size, dm_data) in functions_by_stepsize:
-            j.errprint("Plotting for size", size)
-            ax.plot(dm_data[test["name"]], label="dm %f" % size,
-                    color=(size/maxsize, 0.5, 0.5))
-        fig.savefig(path.join(plot_dir, test["name"] + ".eps"))
-        plt.close(fig) # close the figure to save memory
-
 def solved_vs_iterations_plots(data_dir,
         iterations_count=iterations_config["end"],
         runs_count=experiment_defaults["runs"]):
