08df1588abedf8117022c9ba638712a94c6db642
Jacob Errington <nictytan@gmail.com>
2015-01-03 22:33:12 -0500

    use dictionary for subprocess function and improve CLI
    By using a dictionary to convey keyword arguments instead of a tuple to
    convey positional arguments, I gain in extensibility.
    
    The command-line interface now includes several switches to control the
    behaviour of the data generation process.

--- generate_data.py
+++ generate_data.py
@@ -20,20 +20,33 @@ RUNS_COUNT = 50
 
 NITERS = [5, 10, 50, 100, 250, 500]
 DISTS = [5, 10, 100, 500, 1000, 10000]
+ALL_SOLVERS = ["dm", "sa"]
 
 DEFAULT_RESULTS_DIR = "/home/tsani/projects/dm/results"
 
-def do_test(args):
-    experiment_dir, solver_name, test = args
-    solver = getattr(dms, solver_name)
-    solver_dir = jt.mkdir_p(path.join(experiment_dir, solver_name))
+def do_test(kwargs):
+    experiment_dir, solver, test = [kwargs[s] for s in ["experiment_dir", "solver", "test"]]
+
+    def arg_default(arg_name, default_value):
+        try:
+            v = kwargs[arg_name]
+        except KeyError:
+            v = default_value
+        return v
+
+    niters = arg_default("niters", NITERS)
+    dists = arg_default("dists", DISTS)
+    options = arg_default("options", {})
+
+    solver_function = getattr(dms, solver["name"])
+    solver_dir = jt.mkdir_p(path.join(experiment_dir, solver["name"]))
 
     print("begin", test["name"], sep='\t', file=sys.stderr)
 
     test_dir = jt.mkdir_p(path.join(solver_dir, test["name"]))
-    for niter in NITERS:
+    for niter in niters:
         niter_dir = jt.mkdir_p(path.join(test_dir, str(niter)))
-        for dist in DISTS:
+        for dist in dists:
             dist_dir = jt.mkdir_p(path.join(niter_dir, str(dist)))
 
             if path.exists(path.join(dist_dir, "result")):
@@ -45,16 +58,17 @@ def do_test(args):
 
             try:
                 start_time = time()
-                results = [solver(
+                results = [solver_function(
                             fun,
                             niter,
                             dim=dim,
-                            distance=dist)
+                            distance=dist,
+                            **options)
                         for _ in xrange(RUNS_COUNT)]
                 end_time = time()
                 average_time = (end_time - start_time) / RUNS_COUNT
             except Exception as e:
-                print("optimization failed:", solver_name, test["name"], niter,
+                print("optimization failed:", solver["name"], test["name"], niter,
                         dist)
             else:
                 summary = {
@@ -62,7 +76,7 @@ def do_test(args):
                             "average_time": average_time
                         },
                         "settings": {
-                            "solver": solver_name,
+                            "solver": solver["name"],
                             "test": test["name"],
                             "niter": niter,
                             "distance": dist,
@@ -75,15 +89,25 @@ def do_test(args):
 
     print("end", test["name"], sep='\t', file=sys.stderr)
 
-def main(experiment_dir):
-    for solver_name in ["dm", "bh"]:
+def main(kwargs):
+    experiment_dir = kwargs["experiment_dir"]
+
+    for solver in kwargs["solvers"]:
         pool = mp.Pool(3)
-        pool.map(do_test, zip(repeat(experiment_dir), repeat(solver_name),
-            tf.tests_list))
+        pool.map(do_test, map(
+            lambda test: {
+                "test": test,
+                "experiment_dir": experiment_dir,
+                "solver": solver
+            }, kwargs["tests"]))
+
 
 if __name__ == "__main__":
     experiment_dir = None
     experiment_dir_suffix = None
+    solver_names = []
+    tests = []
+    options = {}
 
     i = 1
     while i < len(sys.argv):
@@ -95,13 +119,62 @@ if __name__ == "__main__":
         elif arg == '--suffix':
             experiment_dir_suffix = n()
             i += 1
+        elif arg == '--solvers':
+            solver_names = n().split(',')
+            i += 1
+        elif arg == '--tests':
+            tests = n().split(',')
+            i += 1
+        elif arg == '--options':
+            options = json.loads(n())
+            i += 1
         i += 1
 
-    experiment_dir = experiment_dir or \
-            jt.mkdir_p(path.join(
-                DEFAULT_RESULTS_DIR, str(datetime.now())))
+    # default: in the default results dir (global constant), make a directory
+    # named with the current date and time
+    experiment_dir = experiment_dir or jt.mkdir_p(
+            path.join(DEFAULT_RESULTS_DIR, str(datetime.now())))
 
+    solver_names = solver_names or ALL_SOLVERS # default: assume all solvers
+    for s in solver_names:
+        if s not in ALL_SOLVERS:
+            print("Solver", s, "not implemented.", file=sys.stderr)
+            sys.exit(1)
+
+    # Make a list of solver dicts, with their name and any options if given on
+    # the command line
+    solvers = []
+    for solver_name in solver_names:
+        try:
+            solver_options = options[solver_name]
+        except KeyError:
+            solver_options = {}
+
+        solvers.append({
+            "name": solver_name,
+            "options": solver_options
+        })
+
+    # Add the suffix if there is one.
     if experiment_dir_suffix is not None:
         experiment_dir += "-" + experiment_dir_suffix
 
-    main(experiment_dir)
+    if tests: # if a list of test-functions was given on the command line, then
+        # get the entries from the test-functions dictionary
+        tests_ = []
+        for t in tests:
+            try:
+                tests_.append(tf.tests_dict[t])
+            except KeyError:
+                print("Objective function", t, "is not implemented.", file=sys.stderr)
+                sys.exit(1)
+        tests = tests_
+    else:
+        tests = tf.tests_list # default: assume all tests
+
+    main({
+        "experiment_dir": experiment_dir,
+        "options": options,
+        "tests": tests,
+        "solvers": solvers
+    })
