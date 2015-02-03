d8c12593fcfc717a2f761b99ec3fed65778ff4d3
Jacob Errington <jacob.errington@mail.mcgill.ca>
2015-02-03 13:49:53 -0500

    add M implementation of schwefel function

--- DMTestFunctions.m
+++ DMTestFunctions.m
@@ -11,4 +11,7 @@ griewankN[v_List /; And @@ Map[NumberQ, v]] :=
 rosenbrockN[v_List /; And @@ Map[NumberQ, v]] :=
     Sum[(1 - v[[i]])^2 + 100 (v[[i+1]] - v[[i]]^2)^2, {i, 1, Length[v] - 1}];
 
+schwefelN[v_List /; And @@ Map[NumberQ, v]] :=
+    418.9828872724339 Length[v] - Sum[v[[i]] Sin[Sqrt[Abs[v[[i]]]]], {i, 1, Length[v]}];
+
 EndPackage[];
