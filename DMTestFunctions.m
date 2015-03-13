f2a31c1b8f62e7022dd94723bcbbc18761ca284b
Jacob Errington <nictytan@gmail.com>
2015-03-13 04:33:56 -0400

    use Block to isolate local variables where possible in test functions

--- DMTestFunctions.m
+++ DMTestFunctions.m
@@ -1,7 +1,7 @@
 BeginPackage["DMTestFunctions`"];
 
 griewankN[v_List /; And @@ Map[NumberQ, v]] :=
-    Module[{a},
+    Block[{a},
         a = 1 / 4000 v . v
             - Times @@ MapIndexed[Cos[#1 / Sqrt[#2[[1]]]] &, v]
             + 1;
@@ -18,9 +18,13 @@ schwefelN[v_List /; And @@ Map[NumberQ, v]] :=
         - Sum[v[[i]] Sin[Sqrt[Abs[v[[i]]]]], {i, 1, Length[v]}];
 
 schafferN[v_List /; And @@ Map[NumberQ, v]] :=
-    Sum[(v[[i]]^2 + v[[i+1]]^2)^0.25
-        * (Sin[50 (v[[i]]^2 + v[[i+1]]^2)^0.10]^2 + 1),
-        {i, 1, Length[v] - 1}];
+    Block[{i},
+        Sum[(v[[i]]^2 + v[[i+1]]^2)^0.25
+            * (Sin[50 (v[[i]]^2 + v[[i+1]]^2)^0.10]^2 + 1),
+            {i, 1, Length[v] - 1}]
+    ];
+
+
 
 EndPackage[];
 
