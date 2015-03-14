63873d4713d68c8e208c72070a7f079c414d855e
Jacob Errington <nictytan@gmail.com>
2015-03-14 13:41:14 -0400

    get rid of evalN Sow-Reap trick for counting function evaluations
    In the Before Times, when we didn't know about the EvaluationMonitor
    option for NMinimize and FindMinimum, we counted function evaluations by
    embedding a Sow call in the objective function, which were then reaped
    by the caller. Due to the way the evaluations were counted, this caused
    errors when the caller didn't set up the Reap environment correctly
    (namely with an additional global variable called evalN).

--- DMTestFunctions.m
+++ DMTestFunctions.m
@@ -1,14 +1,8 @@
 BeginPackage["DMTestFunctions`"];
 
-griewankN[v_List /; And @@ Map[NumberQ, v]] :=
-    Block[{a},
-        a = 1 / 4000 v . v
+griewankN[v_List /; And @@ Map[NumberQ, v]] := 1 / 4000 v . v
             - Times @@ MapIndexed[Cos[#1 / Sqrt[#2[[1]]]] &, v]
             + 1;
-        Sow[{evalN, a}];
-        evalN += 1;
-        a
-    ];
 
 rosenbrockN[v_List /; And @@ Map[NumberQ, v]] :=
     Sum[(1 - v[[i]])^2 + 100 (v[[i+1]] - v[[i]]^2)^2, {i, 1, Length[v] - 1}];
