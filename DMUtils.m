cb87f82bea272ba491afeb29b7d548f2f0a46417
Jacob Errington <jacob.errington@mail.mcgill.ca>
2015-04-09 16:19:38 -0400

    remove trailing whitespace from DMUtils.m

--- DMUtils.m
+++ DMUtils.m
@@ -1,11 +1,11 @@
 BeginPackage["DMUtils`"];
 
-RandomAwayFrom[point_, distance_] := 
+RandomAwayFrom[point_, distance_] :=
     With[{rvec = Map[RandomReal[{-1, 1}] &, point]},
         point + (distance / Norm[rvec]) rvec
     ];
 
-ShiftOverBy[vect_, fun_] := fun[# - vect] &; 
+ShiftOverBy[vect_, fun_] := fun[# - vect] &;
 
 ParseNumber[s_] := With[{ss = StringToStream[s]},
     n = Read[ss, Number];
